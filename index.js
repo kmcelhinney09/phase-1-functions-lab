d0462558f84000f62b81029577e3f01a812345e2
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-12 17:47:07 -0700

    commit tree 4bff0004f45b376dfa7698468945aeb4ef313b8b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start, end= 42){
+    const blocks = start >= end? start - end: end - start;
+    return blocks;
+}
+
+function distanceFromHqInFeet(start,end=42){
+    return  distanceFromHqInBlocks(start,end) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+    return distanceFromHqInFeet(start, end)
+}
+
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start, end);
+    console.log(distance);
+    if (distance <= 400){
+        return 0;
+    } 
+    else if(distance > 400 && distance <= 2000){
+        return (distance-400)*0.02;
+    }
+    else if(distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
+
